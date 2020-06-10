b7bcbb34304105696601c4e348dd9857a6625d21
sfu420 <freedom.of.p2p@gmail.com>
2020-06-10 12:35:14 +0200

    Update Print usage task

--- src/todo.java
+++ src/todo.java
@@ -3,14 +3,18 @@ public class todo {
   public static void main(String[] args) {
 
     if (args.length == 0) {
-      String help = "\nCommand Line Todo application\n" +
-          "=============================\n\n" +
-          "Command line arguments:\n" +
-          "\t-l   Lists all the tasks\n" +
-          "\t-a   Adds a new task\n" +
-          "\t-r   Removes an task\n" +
-          "\t-c   Completes an task";
-      System.out.println(help);
+      printHelp();
     }
+  }
+
+  public static void printHelp() {
+    String help = "\nCommand Line Todo application\n" +
+        "=============================\n\n" +
+        "Command line arguments:\n" +
+        "\t-l   Lists all the tasks\n" +
+        "\t-a   Adds a new task\n" +
+        "\t-r   Removes an task\n" +
+        "\t-c   Completes an task";
+    System.out.println(help);
   }
 }
