37dbf4b406cf10410d94bac8e8a7d79396e4ebf0
sfu420 <freedom.of.p2p@gmail.com>
2020-06-10 14:15:55 +0200

    Add List tasks part

---
+++ src/FileOPs.java
@@ -0,0 +1,45 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileOPs {
+  protected List<String> lines = new ArrayList<>();
+  protected final String filename = "tasks.txt";
+
+  public List<String> readFile() {
+    try {
+      Path filePath = Paths.get("./" + this.filename);
+      this.lines = Files.readAllLines(filePath);
+    } catch (IOException e) {
+      System.out.println("The file does not exist");
+      System.exit(2);
+    }
+    return this.lines;
+  }
+
+  public void writeFile(List<String> content) {
+    try {
+      Path filePath = Paths.get("./" + this.filename);
+      Files.write(filePath, content);
+    } catch (IOException e) {
+      System.out.println("Can't write file. Please check permission");
+      System.exit(2);
+    }
+  }
+
+  public void printContent(List<String> content) {
+    for (String line : content) {
+      System.out.println(line);
+    }
+  }
+
+  public void fillFile() {
+    this.lines.add((this.lines.size() + 1) + " - " + "Walk the dog");
+    this.lines.add((this.lines.size() + 1) + " - " + "Bux Milk");
+    this.lines.add((this.lines.size() + 1) + " - " + "Do homework");
+    writeFile(this.lines);
+  }
+}
--- src/todo.java
+++ src/todo.java
@@ -1,10 +1,16 @@
 public class todo {
 
   public static void main(String[] args) {
+    FileOPs fIO = new FileOPs();
+
+    fIO.fillFile();
 
     if (args.length == 0) {
       printHelp();
+    } else if (args[0].equals("-l")) {
+      fIO.printContent(fIO.readFile());
     }
+
   }
 
   public static void printHelp() {
@@ -13,8 +19,10 @@ public class todo {
         "Command line arguments:\n" +
         "\t-l   Lists all the tasks\n" +
         "\t-a   Adds a new task\n" +
-        "\t-r   Removes an task\n" +
-        "\t-c   Completes an task";
+        "\t-r   Removes a task\n" +
+        "\t-c   Completes a task";
     System.out.println(help);
   }
+
+
 }
